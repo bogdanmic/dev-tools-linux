e3010c0ad5cb1fe2c87dff912e8416967968acbc
bogdanmic <mic.bogdan@gmail.com>
2019-03-02 10:29:16 +0200

    Small cleanup

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        darkTheme: true,
         webPreferences: {
             nodeIntegration: true
         }
@@ -40,17 +41,17 @@ app.on('window-all-closed', function () {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-      app.quit()
+        app.quit()
     }
-  })
-  
-  app.on('activate', function () {
+})
+
+app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-      createWindow()
+        createWindow()
     }
-  })
-  
+})
+
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
