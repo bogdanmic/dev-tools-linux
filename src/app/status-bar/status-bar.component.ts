719070077eed2af6de88abe080be29f6f6c45d2e
bogdanmic <mic.bogdan@gmail.com>
2019-04-13 16:25:07 +0300

    Add the snack bar functionality to the status bar.

--- src/app/status-bar/status-bar.component.ts
+++ src/app/status-bar/status-bar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../services/electron.service';
+import { MatSnackBar } from '@angular/material';
+import { InterProcessSyncService } from '../services/inter-process-sync.service';
+import { EventResponse } from '../common/event-response';
+import { ToastNotificationComponent } from '../toast-notification/toast-notification.component';
 
 declare const window: any;
 
@@ -11,8 +15,13 @@ declare const window: any;
 export class StatusBarComponent implements OnInit {
   isLinux: boolean = false;
   isOnline: boolean = true;
+  durationInSeconds: number = 5;
 
-  constructor(private es: ElectronService) { }
+  constructor(
+    private es: ElectronService,
+    private snackBar: MatSnackBar,
+    private ips: InterProcessSyncService
+  ) { }
 
   ngOnInit() {
     this.isLinux = this.es.isLinux;
@@ -22,6 +31,15 @@ export class StatusBarComponent implements OnInit {
     })
     window.addEventListener('offline', () => {
       this.isOnline = navigator.onLine
+    })
+
+    this.ips.interProcessMessage.subscribe((message: EventResponse) => {
+      if (message) {
+        this.snackBar.openFromComponent(ToastNotificationComponent, {
+          data: message,
+          duration: this.durationInSeconds * 1000,
+        });
+      }
     })
   }
 
