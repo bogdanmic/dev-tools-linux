312b45c65fb86f6e8db5b87dba702bce9ab341c1
bogdanmic <mic.bogdan@gmail.com>
2019-04-13 15:21:44 +0300

    Make some properties nullable

--- electron/src/common/event-response.ts
+++ electron/src/common/event-response.ts
@@ -4,21 +4,21 @@ export class EventResponse {
 
     private _event: AppEvent;
     private _successful: boolean;
-    private _value: string;
-    private _message: string;
+    private _value: string | null;
+    private _message: string | null;
 
-    constructor(event: AppEvent, successful: boolean, value: string, message: string) {
+    constructor(event: AppEvent, successful: boolean, value: string | null, message: string | null) {
         this._event = event
         this._successful = successful
         this._value = value
         this._message = message
     }
 
-    public get message(): string {
+    public get message(): string | null {
         return this._message;
     }
 
-    public get value(): string {
+    public get value(): string | null {
         return this._value;
     }
 
