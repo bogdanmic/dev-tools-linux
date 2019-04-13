017f51faa6a9de45fb4df352ef6657157d77406b
bogdanmic <mic.bogdan@gmail.com>
2019-04-13 15:22:26 +0300

    Implement functionality to select folder and test if we have write access in it.

--- electron/src/tasks/impl/select-work-dir.task.ts
+++ electron/src/tasks/impl/select-work-dir.task.ts
@@ -1,11 +1,48 @@
 import { Task } from '../task.interface';
 import { AppEvent } from '../../common/app-event';
 import { EventResponse } from '../../common/event-response';
+import * as path from 'path'
 
-export default class SelectWorkDir implements Task{
+const { dialog } = require('electron').remote
+const fileSystem = require('electron').remote.require('fs')
+
+export default class SelectWorkDir implements Task {
     execute(event: AppEvent): EventResponse {
-        let response = new EventResponse(event, true, event, `DemoTask(${event}-${event}):SelectWorkDir(${event})!`);
-        return response
+        let successful: boolean = false
+        let message: string | null = null
+        let workDir: string | null = null
+
+        let selectedDir = dialog.showOpenDialog({ properties: ['openDirectory'] });
+        if (selectedDir) {
+            workDir = selectedDir[0]
+            message = `Selected work directory [${workDir}]`
+            successful = true
+        } else {
+            message = "Select a valid directory!"
+        }
+
+        // Now that we selected a working directory we can check if it's writeable
+        if (successful && workDir != null) {
+            // fileSystem.access(path.join(workDir), fileSystem.constants.W_OK, function (err: any) {
+            //     if (err) {
+            //         console.error("can't write");
+            //         process.exit(1);
+            //     }
+            //     console.log("can write");
+            //     process.exit(0);
+            // });
+            // I tried to use the solution above to check for write access but for some reason it did not work
+            // The solution above works if it's executed in the main thread.
+            try {
+                fileSystem.writeFileSync(path.join(workDir, 'test-write-access'))
+                fileSystem.unlinkSync(path.join(workDir, 'test-write-access'))
+            } catch (e) {
+                successful = false
+                message = `Selected work directory [${workDir}] is not writeable!`
+            }
+        }
+
+        return new EventResponse(event, successful, workDir, message);
     }
 
 }
