032cf2546cd82b28cb216b05c05b5dd1a39cae61
bogdanmic <mic.bogdan@gmail.com>
2019-04-14 16:58:56 +0300

    Create the functionality of the init script to prepare the directories we will need

--- electron/src/tasks/impl/init-work-dir.task.ts
+++ electron/src/tasks/impl/init-work-dir.task.ts
@@ -2,19 +2,55 @@ import { Task } from '../task.interface';
 import { EventResponse } from '../../common/event-response';
 import * as child from 'child_process'
 import { AppEventPayload } from '../../common/app-event-payload';
+import { AppConstants } from '../../common/app-constants';
 
 const { dialog } = require('electron').remote
 const fileSystem = require('electron').remote.require('fs')
 
 export default class InitWorkDir implements Task {
     execute(eventPayload: AppEventPayload): EventResponse {
-        let successful: boolean = false
-        let message: string | null = null
-        let workDir: string | null = null
+        let workDir: string = eventPayload.data
 
-        child.exec('echo "I just did something!' + eventPayload.data + ' `date`" >> lol2.txt')
+        let successful: boolean = true
+        let message: string | null = `[${workDir}] initialized`
 
-        return new EventResponse(eventPayload.event, successful, workDir, message);
+        try {
+            let isDirectory: boolean = fileSystem.lstatSync(workDir).isDirectory()
+            if (!isDirectory) {
+                successful = false
+                message = `[${workDir}] is not a directory`
+            } else {
+                child.exec(`mkdir -p ${workDir}${AppConstants.SETUP_PATH_PRIVATE}`, (error, stdout, stderr) => {
+                    if (error) {
+                        successful = false
+                        message = `Could not create [${workDir}${AppConstants.SETUP_PATH_PRIVATE}] directory. Error: ${stderr}`
+                    }
+                });
+                child.exec(`mkdir -p ${workDir}${AppConstants.SETUP_PATH_CONTAINERS}`, (error, stdout, stderr) => {
+                    if (error) {
+                        successful = false
+                        message = `Could not create [${workDir}${AppConstants.SETUP_PATH_CONTAINERS}] directory. Error: ${stderr}`
+                    }
+                });
+                child.exec(`mkdir -p ${workDir}${AppConstants.SETUP_PATH_TOOLS}`, (error, stdout, stderr) => {
+                    if (error) {
+                        successful = false
+                        message = `Could not create [${workDir}${AppConstants.SETUP_PATH_TOOLS}] directory. Error: ${stderr}`
+                    }
+                });
+                child.exec(`mkdir -p ${workDir}${AppConstants.SETUP_PATH_WORK}`, (error, stdout, stderr) => {
+                    if (error) {
+                        successful = false
+                        message = `Could not create [${workDir}${AppConstants.SETUP_PATH_WORK}] directory. Error: ${stderr}`
+                    }
+                });
+            }
+        } catch (e) {
+            successful = false
+            message = `An error occurred ${e}`
+        }
+
+        return new EventResponse(eventPayload.event, successful, null, message);
     }
 
 }
